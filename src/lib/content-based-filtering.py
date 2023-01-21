de2563547dd00f65f14c34c399956087497b06d1
s-angheben <samuele.angheben@gmail.com>
2023-01-21 10:41:57 +0100

    wrote some function

--- src/lib/content-based-filtering.py
+++ src/lib/content-based-filtering.py
@@ -1,1 +1,38 @@
-## TODO
+from pyspark.ml import Pipeline
+from pyspark.ml.feature import HashingTF, MinHashLSH
+
+from utils import *
+from data_utils import *
+
+# https://spark.apache.org/docs/2.2.3/ml-features.html#minhash-for-jaccard-distance
+
+
+
+def create_result_set_dataframe(sc, query_set, relational_table):
+    query_ids   = []
+    result_sets = []
+    for row in query_set.rdd.toLocalIterator():
+        print(row)
+        print(row.query_string)
+        query_ids.append(row.query_id)
+
+        result_set = get_query_result_set_ids(sc, row.query_string, relational_table)
+        result_sets.append(result_set.collect())
+
+    result_set_df = sc.createDataFrame(zip(query_ids, result_sets),
+                                       ['query_id', 'result_set'])
+    result_set_df.show()
+    result_set_df.printSchema()
+
+
+def test():
+    sc = init_spark("content-based-filtering")
+    utility_matrix = load_utility_matrix(sc)
+    relational_table = load_relational_table(sc)
+    query_set = load_query_set(sc)
+
+    create_result_set_dataframe(sc, query_set, relational_table)
+
+
+if __name__ == "__main__":
+    test()
--- src/lib/data_utils.py
+++ src/lib/data_utils.py
@@ -1,4 +1,5 @@
 from utils import *
+import re
 
 def load_relational_table(sc):
     relational_table = sc.read.option("header",True)\
@@ -32,6 +33,8 @@ def load_query_set(sc):
     df2 = df2.withColumn("query_param", F.expr("slice(allinfo, 2, SIZE(allinfo))"))
     # create the query_string concatenated with AND and save it in query_string
     df2 = df2.withColumn("query_string", F.concat_ws(" AND ",F.col("query_param")))
+
+
     df2 = df2.drop("allinfo")
 
     # print(df2.first().query_string)
@@ -39,15 +42,24 @@ def load_query_set(sc):
     # df2.show()
     return df2
 
-def get_query_result_set_all(sc, query_string, relational_table):
+
+def get_query_result_set_ids(sc, query_string, relational_table):
+    # add quotes to values between AND
+    query_string = re.sub(r'(=)\s*(.*?) AND', r'\1"\2" AND', query_string)
+    # add quotes to the last value
+    tmp = query_string.split("=")
+    last_value = tmp.pop()
+    last_value = '"' + last_value + '"'
+    tmp.append(last_value)
+    query_string = "=".join(tmp)
+    print(query_string)
+
     relational_table.createOrReplaceTempView("items")
     result_set = sc.sql("SELECT * FROM items WHERE " + query_string)
+
     return result_set
 
-def get_query_result_set_id(sc, query_string, relational_table):
-    relational_table.createOrReplaceTempView("items")
-    result_set = sc.sql("SELECT business_id FROM items WHERE " + query_string)
-    return result_set
+
 
 def test():
     sc = init_spark("data_utils")
@@ -58,7 +70,8 @@ def test():
 
     ## example query
     query1 = query_set.first()
-    rs = get_query_result_set_id(sc, query1.query_string, relational_table)
+    rs = get_query_result_set_ids(sc, query1, relational_table)
+    print(type(rs.collect()))
     print(rs.count())
     rs.show()
 
