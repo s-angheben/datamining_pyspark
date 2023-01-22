f7f9394680b3fdcb1ea04ddbcae28fd6895f949b
Marco Frassoni <marco.frassoni@apss.tn.it>
2023-01-22 20:51:50 +0100

    iterating over users and queries

--- src/lib/query_rec.py
+++ src/lib/query_rec.py
@@ -16,18 +16,32 @@ def query_recommendation():
 
     model, df = prepare_model(utility_matrix)
 
-    # Iterate over all users and all queries in order to fulfill the utility matrix
-    # for u in user_set.rdd.toLocalIterator():
-        # for q in query_set.rdd.toLocalIterator():
-            # print(u.id, u)
-            # user = sc.sql("SELECT {} FROM utility_matrix WHERE utility_matrix.user_id = '{}'".format(q.id, u.id))
-            # user = user.withColumn('user_id', F.lit('TEST'))
-            # print(user.head())
+    with open(data_path + 'utility_matrix_filled.csv', 'w') as f_out:
+        writer = csv.writer(f_out, delimiter=',')
+        writer.writerow(utility_matrix.columns)
 
-            # TODO: retrieve key and nearest neighbors
-            # key = df.first()
-            # sim_users = approx_nearest_neighbors(model, df, key, 30)
-            # print(sim_users.head())
+        # Iterate over all users and all queries in order to fulfill the utility matrix
+        count_users = 0
+        for u in user_set.rdd.toLocalIterator():
+            row = [u.id]
+            for q in query_set.rdd.toLocalIterator():
+                # print(u.id, u)
+                rs_value = sc.sql("SELECT utility_matrix.{} FROM utility_matrix WHERE utility_matrix.user_id = '{}'".format(q.query_id, u.id))
+                value = rs_value.collect()[0][0]
+                if value is not None and value != '':
+                    row.append(value)
+                else:
+                    row.append('')
+                    # TODO: retrieve key and nearest neighbors
+                    # key = df.first()
+                    # sim_users = approx_nearest_neighbors(model, df, key, 30)
+                    # print(sim_users.head())
+
+            writer.writerow(row)
+            count_users += 1
+
+            if count_users % 100 == 0:
+                print('Processed {} users'.format(count_users))
 
     # print(utility_matrix.head())
 
