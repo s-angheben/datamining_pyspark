3eac60d3604825aed601d9f154d84ef7388748e9
Marco Frassoni <marco.frassoni@apss.tn.it>
2023-01-22 21:44:30 +0100

    retrieving user's top 30 neighbors

--- src/lib/query_rec.py
+++ src/lib/query_rec.py
@@ -3,6 +3,15 @@ from data_utils import *
 from utils import init_spark, end_session
 
 
+def get_neighbors(df, model, u):
+    user_key = df.where(F.col("user_id") == u.id).first()
+    num_neighbors = 30
+    neighbors = approx_nearest_neighbors(model, df, user_key, num_neighbors)
+    # print(neighbors.show())
+    # Retrieve neighbors' user_id filtering out the user_id of the current user
+    return neighbors.select('user_id').filter(neighbors.user_id != u.id).collect()
+
+
 def query_recommendation():
     # Init spark session
     sc = init_spark("query_recommendation")
@@ -23,24 +32,34 @@ def query_recommendation():
         # Iterate over all users and all queries in order to fulfill the utility matrix
         count_users = 0
         for u in user_set.rdd.toLocalIterator():
+
+            # Row to be printed in the fulfilled utility matrix
+
+            # Retrieve similar users
+            neighbors_ids = get_neighbors(df, model, u)
+
+            # Row to be printed out to the csv file, one per user
             row = [u.id]
+
+            # Iterate over queries
             for q in query_set.rdd.toLocalIterator():
-                # print(u.id, u)
-                rs_value = sc.sql("SELECT utility_matrix.{} FROM utility_matrix WHERE utility_matrix.user_id = '{}'".format(q.query_id, u.id))
+
+                # Lookup into the dataframe for the rating (if any) given by user  u.id to query q.query_id
+                rs_value = utility_matrix.where(F.col("user_id") == u.id).select(F.col(q.query_id))
                 value = rs_value.collect()[0][0]
+
                 if value is not None and value != '':
                     row.append(value)
                 else:
+                    # TODO: recommendation by retrieving neighbors' ratings for the
+                    # given query_id and computing the average
                     row.append('')
-                    # TODO: retrieve key and nearest neighbors
-                    # key = df.first()
-                    # sim_users = approx_nearest_neighbors(model, df, key, 30)
-                    # print(sim_users.head())
 
             writer.writerow(row)
             count_users += 1
+            print(count_users)
 
-            if count_users % 100 == 0:
+            if count_users % 10 == 0:
                 print('Processed {} users'.format(count_users))
 
     # print(utility_matrix.head())
