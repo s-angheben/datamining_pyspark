128c7b3e037f32abe3f3a15a6385829a626fbaa7
Marco Frassoni <marco.frassoni@apss.tn.it>
2023-01-22 17:20:45 +0100

    refactoring

--- src/lib/collaborative_filtering.py
+++ src/lib/collaborative_filtering.py
@@ -11,7 +11,7 @@ def approx_nearest_neighbors(model, rs, key, num=20):
     print("Approximately searching rs for {} nearest neighbors of the user: {}".format(num, key.user_id))
     sim_users = model.approxNearestNeighbors(rs, key.ratings_vector, num)
 
-    print(sim_users.head())
+    # print(sim_users.head())
 
     return sim_users
 
@@ -25,37 +25,25 @@ def add_ratings_vector_format(result_set_df):
 
     result = result_set_df.withColumn("ratings_vector", new_f(F.col("norm_ratings"), F.size(F.col("norm_ratings"))))
 
-    # new_f = F.udf(Vectors.dense, VectorUDT())
-    # result = ratings_vector_df.withColumn("ratings_vector", new_f(F.col("ratings_vector")))
-
     return result
 
 
-class CollaborativeFiltering(object):
+def prepare_model(utility_matrix, bucket_len=2.0, num_hash_tables=3):
+    ut = utility_matrix_create_array(utility_matrix)
+    # print(ut.head())
 
-    def __init__(self, sc_):
-        self.sc = sc_
-        self.queries = {}
+    rs = add_ratings_vector_format(ut)
+    # print(rs.head())
 
-    def prepare_model(self):
-        utility_matrix = load_utility_matrix(self.sc)
-        ut = utility_matrix_create_array(utility_matrix)
-        # print(ut.head())
+    q1 = rs.first().ratings_vector
+    # print(rs.first().user_id, q1)
 
-        item_size = ut.count()
+    brp = BucketedRandomProjectionLSH(inputCol="ratings_vector", outputCol="hashes", bucketLength=bucket_len,
+                                      numHashTables=num_hash_tables)
+    model = brp.fit(rs)
 
-        rs = add_ratings_vector_format(ut)
-        # print(rs.head())
+    # Feature Transformation
+    print("The hashed dataset where hashed values are stored in the column 'hashes':")
+    # model.transform(rs).show()
 
-        q1 = rs.first().ratings_vector
-        print(rs.first().user_id, q1)
-
-        brp = BucketedRandomProjectionLSH(inputCol="ratings_vector", outputCol="hashes", bucketLength=2.0,
-                                          numHashTables=3)
-        model = brp.fit(rs)
-
-        # Feature Transformation
-        print("The hashed dataset where hashed values are stored in the column 'hashes':")
-        # model.transform(rs).show()
-
-        return model, rs
+    return model, rs
--- src/lib/test_collaborative_filtering.py
+++ src/lib/test_collaborative_filtering.py
@@ -1,13 +1,14 @@
-from collaborative_filtering import CollaborativeFiltering, approx_nearest_neighbors
+from collaborative_filtering import approx_nearest_neighbors, prepare_model
+from src.lib.data_utils import load_utility_matrix
 from utils import init_spark, end_session
 
 if __name__ == "__main__":
     # Init spark session
     sc = init_spark("collaborative-filtering")
 
-    cf = CollaborativeFiltering(sc)
+    utility_matrix = load_utility_matrix(sc)
 
-    model, result_set = cf.prepare_model()
+    model, result_set = prepare_model(utility_matrix)
 
     sim_users = approx_nearest_neighbors(model, result_set, result_set.first(), 10)
     print(sim_users.head())
